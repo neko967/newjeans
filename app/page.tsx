33a325712528d65c6de4996247941b6bf25f8432
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2023-10-10 22:44:49 +0900

    フォルダ分け及びUIライブラリの導入

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,21 @@ export default function Home() {
             <code className="font-mono font-bold">api/index.py</code>
           </Link>
         </p>
+        <div>
+          <Link href="/player_a_meiro">
+            player_a_meiro
+          </Link>
+        </div>
+        <div>
+          <Link href="/player_b_meiro">
+            player_b_meiro
+          </Link>
+        </div>
+        <div>
+          <Link href="/pointer">
+            pointer
+          </Link>
+        </div>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
